1d12b5b5c00da9927d89a8bbacf67041a088532e
Elsa Laussucq <elsa.laussucq@epita.fr>
2022-10-26 20:29:41 +0200

    [ADD][LEVELI] Distance of Levenshtein

--- LevelI/Difference.php
+++ LevelI/Difference.php
@@ -28,19 +28,22 @@ class Difference
      *
      * @return mixed
      */
-    public function whatIsTheCostPlease()
+    public function whatIsTheCostPlease($a, $b)
     {
         // @ TODO
         $lenA = strlen($this->a);
         $lenB = strlen($this->b);
-
-        for ($i = 1; $i <= $lenA; ++$i) {
-            for ($j = 1; $j <= $lenB; ++$j) {
+        $dis = range(0,$lenB);
+        
+        for ($i = 1; $i <= $lenA; $i++) {
+            $dis_new[0] = $i;
+            for ($j = 1; $j <= $lenB; $j++) {
                 $c = ($this->a[$i - 1] === $this->b[$j - 1]) ? 0 : 1;
-                $matrix[$i][$j] = 0;
+                $dis_new[$j] = min($dis[$j] + 1, $dis_new[$j - 1] + 1, $dis[$j - 1] + $c);
             }
+            $dis = $dis_new;
         }
 
-        return $matrix[$lenA][$lenB];
+        return $dis[$lenB];
     }
 }
